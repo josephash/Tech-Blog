04d7c20c7350b0372215afedb0fe58a2cb991372
Sam Joseph <josephash1818@gmail.com>
2023-07-26 14:48:24 -0400

    starter code

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+let sequelize;
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+	sequelize = new Sequelize({
+		database: process.env.DB_NAME,
+		username: process.env.DB_USER,
+		password: process.env.DB_PW,
+		host: 'localhost',
+		dialect: 'mysql',
+		port: 3306
+	});
+}
+
+module.exports = sequelize;
