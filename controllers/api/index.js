4b31a15af8afe94200b0b0b5b8528d83a9cc2066
Sam Joseph <josephash1818@gmail.com>
2023-07-27 17:11:19 -0400

    import fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
-const commentRoutes = require('./comment-routes.js');
+const userRoutes = require('./userRoutes.js');
+const postRoutes = require('./blogRoutes.js');
+const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
