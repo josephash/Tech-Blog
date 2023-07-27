6d4be3f899497db59dffc77b1e3eb9bf18745782
Sam Joseph <josephash1818@gmail.com>
2023-07-27 14:33:02 -0400

    web routes

--- controllers/webRoutes.js
+++ controllers/webRoutes.js
@@ -1,3 +1,51 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+	try {
+		res.render('home', {
+
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/dashboard', (req, res) => {
+	try {
+		res.render('dashboard', {
+			
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/login', (req, res) => {
+	try {
+		res.render('login', {
+			
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/signup', (req, res) => {
+	try {
+		res.render('signup', {
+			
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/logout', (req, res) => {
+	try {
+		
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
